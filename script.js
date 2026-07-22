5db8b04546800c049c9721245eb96ee9ebef7c70
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-07-23 00:20:24 +0500

    Form

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
- let forms =  document.getElementById('formm');
+let forms = document.getElementById('formm');
 
- forms.addEventListener("submit" , function(events){
+forms.addEventListener("submit", function (events) {
 
     events.preventDefault();
 
-   let name = document.getElementById('std_name').value;
-   let ftname =  document.getElementById('ft_name').value;
-   let email =  document.getElementById('std_email').value;
-   let course =  document.getElementById('select_course').value;
+    let name = document.getElementById('std_name').value;
+    let ftname = document.getElementById('ft_name').value;
+    let email = document.getElementById('std_email').value;
+    let course = document.getElementById('select_course').value;
 
-   if(name === "" || ftname === "" || email === ""){
-    alert("Please fill this  feilds");
-   }
+    if (name === "" || ftname === "" || email === "") {
+        alert("Please fill this  feilds");
+    }
+     let output = document.getElementById('output_div');
 
-   let output = document.getElementById('output_div');
+        output.innerHTML =
+            "<h2>Registration Successful</h2>" +
+            "<p>Name: " + name + "</p>" +
+            "<p>Father Name: " + ftname + "</p>" +
+            "<p>E-mail: " + email + "</p>" +
+            "<p>Course: " + course + "</p>";
 
-   output.innerHTML =
-    "<h2>" + "Registration Successful" + "</h2>"+
-    "<p>" + "Name: " + name + "</p>"+
-     "<p>" + "FatherName: " + ftname + "</p>"+
-    "<p>" + "E-mail: " + email + "</p>" + 
-    "<p>" + "Course: " + course + "</p>"
+        output.style.display = "block";
+        output.style.backgroundColor = navy;
 
-    
-    output.style.display = "block";
-   
-});
+    });
